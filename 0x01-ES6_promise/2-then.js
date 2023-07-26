d97cf7089956726d74fb000aa72713d2a568a35e
Jaycode068 <ajayijohnson68@yahoo.com>
2023-07-26 09:37:41 +0000

    Using then, catch and finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then((response) => {
-      console.log('Success:', response);
-      return { status: 200, body: 'success' };
-    })
-    .catch((error) => {
-      console.error('Error:', error.message);
-      throw new Error(); // Throw an empty Error object
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
-
